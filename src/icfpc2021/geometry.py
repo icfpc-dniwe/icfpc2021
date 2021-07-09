9d127d6e00e182f94fece8e8fa32d1ad582f83c8
vuvko <notvuvko@gmail.com>
2021-07-09 20:45:06 +0300

    Added bfs search for movable points.

--- src/icfpc2021/geometry.py
+++ src/icfpc2021/geometry.py
@@ -1,9 +1,10 @@
 from numba import njit
 import math
+from queue import Queue
 from shapely.geometry import Polygon, LineString
 from itertools import combinations
-from .types import Point
-from typing import List, Tuple
+from .types import Point, Figure
+from typing import List, Tuple, Generator
 
 
 @njit
@@ -29,5 +30,26 @@ def find_longest_diagonal(polygon: Polygon) -> Tuple[Point, Point]:
     return max_diag
 
 
-def find_longest_vertical(polygon: Polygon) -> Tuple[Point, Point]:
-    pass
+def find_movable_points(figure: Figure, movable_point: int, fixed_points: List[int]) -> List[int]:
+    def find_neighbors(point: int) -> Generator[int, None, None]:
+        for cur_edge in figure.edges:
+            if cur_edge[0] == point:
+                yield cur_edge[1]
+            elif cur_edge[1] == point:
+                yield cur_edge[0]
+
+    watched_points = [False] * len(figure.vertices)
+    watched_points[movable_point] = True
+    for cur_point in fixed_points:
+        watched_points[cur_point] = True
+    result = [movable_point]
+    points_queue = Queue()
+    points_queue.put(movable_point)
+    while not points_queue.empty():
+        cur_point = points_queue.get()
+        for cur_neighbor in find_neighbors(cur_point):
+            if not watched_points[cur_neighbor]:
+                watched_points[cur_neighbor] = True
+                points_queue.put(cur_neighbor)
+                result.append(cur_neighbor)
+    return result
