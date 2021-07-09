6edec9ee397f1d0284a44bc85393084708d0ab0d
vuvko <notvuvko@gmail.com>
2021-07-09 23:03:32 +0300

    small fix

--- src/icfpc2021/solver/brute_folder.py
+++ src/icfpc2021/solver/brute_folder.py
@@ -14,7 +14,7 @@ def next_fold(edges: Sequence[Edge], vertices: Sequence[PointF]) -> Generator[Se
         yield maybe_folded
 
 
-def iterate_folding(problem: Problem, max_iterations: int = 10 ** 5) -> Optional[Sequence[PointF]]:
+def iterate_folding(problem: Problem, max_iterations: int = 10 ** 5) -> Sequence[PointF]:
     cur_iter = -1
     orig_vertices = np.array(problem.figure.vertices, dtype=np.float32)
     cur_verticies = orig_vertices.copy()
@@ -26,7 +26,7 @@ def iterate_folding(problem: Problem, max_iterations: int = 10 ** 5) -> Optional
         for maybe_folded in next_fold(edges, cur_verticies):
             cur_iter += 1
             if cur_iter >= max_iterations:
-                return None
+                return solution_float
             solution = Solution(vertices=[(round(cur_v[0]), round(cur_v[1])) for cur_v in maybe_folded])
             if validate_stretching(problem, solution):
                 try_find = True
