9e6b0a59c5339f6f5dedb89e13827ada0d6adf29
Nikolay Amiantov <ab@fmap.me>
2021-07-09 15:51:40 +0300

    Implement POST

--- src/icfpc2021/client.py
+++ src/icfpc2021/client.py
@@ -5,13 +5,16 @@ class Client:
     def __init__(self, token: str):
         self.token = token
   
-    def call(self, method, path, **kwargs):
+    def _call(self, method, path, **kwargs):
         ret = requests.request(method, f"https://poses.live{path}", headers={"Authorization": f"Bearer {self.token}"}, **kwargs)
         ret.raise_for_status()
         return ret.json()
   
     def hello(self):
-        return self.call("GET", "/api/hello")
+        self._call("GET", "/api/hello")
 
-    def get_problem(self, problem_id):
-        return Problem.from_dict(self.call("GET", f"/api/problems/{problem_id}"))
+    def get_problem(self, problem_id: int):
+        return Problem.from_dict(self._call("GET", f"/api/problems/{problem_id}"))
+
+    def post_solution(self, problem_id: int, solution: Solution):
+        self._call("POST", f"/api/problems/{problem_id}/solutions", json=solution.to_dict())
